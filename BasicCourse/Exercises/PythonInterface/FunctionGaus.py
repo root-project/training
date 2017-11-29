a5096aee50a2d85e78d2e7f4e762d6c9c873a81c
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2017-11-29 19:41:19 +0100

    Add exercise C++ to Python translation (TF1)

---
+++ BasicCourse/Exercises/PythonInterface/FunctionGaus.py
@@ -0,0 +1,13 @@
+
+from ROOT import TH1F, TF1
+
+# Create, fill and draw histogram
+h = TH1F("myTitle", "myHist", 64, -4, 4)
+h.FillRandom("gaus")
+h.Draw()
+
+# Create and draw function in the same canvas
+f = TF1("g", "gaus", -8, 8)
+f.SetParameters(250, 0, 1)
+f.Draw("Same")
+
