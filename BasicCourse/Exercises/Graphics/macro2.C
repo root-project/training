8aefd9be6a9a0ef6727b75cf074c707c0ac7c956
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2017-12-01 11:25:12 +0100

    Replace Int_t and Float_t by int and float

--- BasicCourse/Exercises/Graphics/macro2.C
+++ BasicCourse/Exercises/Graphics/macro2.C
@@ -4,8 +4,8 @@ void macro2(){
    TH2F *h = new TH2F("h","Option COL example ",300,-4,4,300,-20,20);
    h->SetStats(0);
    h->SetContour(200);
-   Float_t px, py;
-   for (Int_t i = 0; i < 25000000; i++) {
+   float px, py;
+   for (int i = 0; i < 25000000; i++) {
       gRandom->Rannor(px,py);
       h->Fill(px-1,5*py);
       h->Fill(2+0.5*px,2*py-10.,0.1);
