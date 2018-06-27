f85fb93a7f5021980893a4cb41b38526ae4efe21
Danilo Piparo <danilo.piparo@cern.ch>
2018-06-27 11:35:58 +0200

    Add tdf examples

--- SummerStudentCourse/Exercises/WorkingWithColumnarData/hsimpleCpp.C
+++ SummerStudentCourse/Exercises/WorkingWithColumnarData/hsimpleCpp.C
@@ -1,4 +1,9 @@
 {
+
+   // Old way
+   auto c = new TCanvas();
+   c->Divide(3,1);
+
    TFile f("hsimple.root");
    TNtuple *ntuple;
    f.GetObject("ntuple", ntuple);
@@ -15,5 +20,25 @@
       if (std::abs(pz) < 2) h.Fill(px + py);
    }
 
-   h.Draw();
+   c->cd(1);
+   h.DrawCopy();
+
+   // New way jitted
+   auto df = ROOT::RDataFrame("ntuple", "hsimple.root");
+   auto h2 = df.Filter("fabs(pz) < 2")
+               .Define("pxpy", "px+py")
+               .Histo1D({"xysum", "XY Sum;The Sum;Number of Entries", 64, -10, 10}, "pxpy");
+
+   c->cd(2);
+   h2->DrawCopy();
+
+   // New way compiled
+   auto df2 = ROOT::RDataFrame("ntuple", "hsimple.root");
+   auto h3 = df2.Filter([](float pz){return fabs(pz) < 2;}, {"pz"})
+                .Define("pxpy", [](float px, float py){return px+py;}, {"px", "py"})
+                .Histo1D({"xysum", "XY Sum;The Sum;Number of Entries", 64, -10, 10}, "pxpy");
+
+   c->cd(3);
+   h3->DrawCopy();
+
 }
