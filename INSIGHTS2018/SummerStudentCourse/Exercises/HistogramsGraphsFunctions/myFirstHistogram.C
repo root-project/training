63aa367382e6380d00520b4103b6aff1f873dcb7
Danilo Piparo <danilo.piparo@cern.ch>
2018-09-18 11:20:31 +0200

    Remove unwanted dir

--- INSIGHTS2018/SummerStudentCourse/Exercises/HistogramsGraphsFunctions/myFirstHistogram.C
+++
@@ -1,8 +0,0 @@
-{
-
-// We create and draw the histogram in the default canvas
-TH1F h("myHist", "myTitle", 64, -4, 4);
-h.FillRandom("gaus");
-h.Draw();
-
-}
--- INSIGHTS2018/SummerStudentCourse/Exercises/readme.md
+++
@@ -1,3 +0,0 @@
-# The exercises for the ROOT basic course
-
-For each of the learning modules a directory containing exercises is present.
