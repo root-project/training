fe54374b77c986aac9b56f413d69c55760db0235
moneta <lorenzo.moneta@cern.ch>
2022-07-08 17:15:06 +0200

    Remove main from simple macro

--- SummerStudentCourse/2022/Exercises/C++Interpreter/myMacro.C
+++ SummerStudentCourse/2022/Exercises/C++Interpreter/myMacro.C
@@ -1,13 +1,6 @@
-#include "TMath.h"
-#include <iostream>
 
 void myMacro ()
 {
-
-  std::cout << "Result is: " << .... << std::endl;
-}
-
-int main() {
-   myMacro();
-   return 0;
+   // WRITE HERE YOUR OWN CODE
+   // like  std::cout << "Result is: ..." <<  std::endl;
 }
